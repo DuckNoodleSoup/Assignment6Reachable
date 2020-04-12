13f0fbc6c442e3a809c568cc1691b2b38eee9840
Shuzo Katayama <shuzokatayama@Shuzos-Air.fios-router.home>
2020-04-12 15:28:33 -0400

    Add Existing File

---
+++ Assignment-6/src/Graph.java
@@ -0,0 +1,32 @@
+public class Graph {
+    
+    public Graph (int numVertices) {
+        // YOUR CODE HERE
+    }
+
+    public void addEdge(int i, int j) {
+        // YOUR CODE HERE
+    }
+
+    public void removeEdge(int i, int j) {
+        // YOUR CODE HERE
+    }
+
+    public boolean hasEdge(int i, int j) {
+        // YOUR CODE HERE
+    }
+
+    public List outEdges(int i) {
+        // YOUR CODE HERE
+    }
+
+    public List inEdges(int i) {
+        // YOUR CODE HERE
+    }
+
+    boolean reachable(int source, int destination) {
+        // YOUR CODE HERE
+        // Should true if destination can be reached from source,
+        // or false otherwise
+    }
+}
---
+++ Assignment-6/src/List.java
@@ -0,0 +1,3 @@
+public class List {
+// YOUR CODE HERE (possible additional classes to be used above)
+}
